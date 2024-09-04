7f2a2113d56bea2cd72f47c8bf8afb5dd667cf21
sidharth tripathi <sidharthtripathi807@gmail.com>
2024-09-04 21:25:07 +0530

    added channel backend feature

--- app/channels/[channelid]/page.tsx
+++ app/channels/[channelid]/page.tsx
@@ -1,22 +1,45 @@
 import { Button } from "@/components/ui/button";
 import { VideoCard } from "@/components/ui/VideoCard";
+import { prisma } from "@/lib/prisma";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
-export default function Channel(){
+export default async function Channel({params : {channelid}} : {params : {channelid : string}}){
+    const creator = await prisma.user.findUnique({
+        where : {username : channelid},
+        select : {
+            avatar : true,
+            converImage : true,
+            username : true,
+            subscribersCount : true,
+            videosPublishedCount : true,
+            videos : {
+                select : {
+                    thumbnailUrl : true,
+                    id : true,
+                    title : true,
+                    views : true,
+                    createdAt : true,
+                }
+            }
+
+        }
+    })
+    if(!creator) return notFound()
     return(
         <div className="container px-2 sm:px-0">
             <header className="space-y-4">
-                <Image alt="banner image" src = '/banner.jpg' width={30} height={30} sizes="100vw" className="rounded-md w-full sm:aspect-[1/0.15] aspect-[1/0.3] object-cover"/>
+                <Image alt="banner image" src = {creator.converImage} width={30} height={30} sizes="100vw" className="rounded-md w-full sm:aspect-[1/0.15] aspect-[1/0.3] object-cover"/>
                 <div className="flex items-center gap-4">
-                    <Image alt="profile" src="/profile.webp" width={300} height={300} className="rounded-full sm:size-44 size-36 object-cover" />
+                    <Image alt="profile" src={creator.avatar} width={300} height={300} className="rounded-full sm:size-44 size-36 object-cover" />
                     <div className="space-y-2">
-                        <h1 className="text-3xl font-extrabold">MailTrap</h1>
+                        <h1 className="text-3xl font-extrabold">{creator.username}</h1>
                         <div className="space-x-2 text-sm text-secondary-foreground/80">
-                            <span>@mailtrap</span>
+                            <span>@{creator.username}</span>
                             <span>&#8226;</span>
-                            <span>130k subscribers</span>
+                            <span>{creator.subscribersCount} subscribers</span>
                             <span>&#8226;</span>
-                            <span>231 videos</span>
+                            <span>{creator.videosPublishedCount} videos</span>
                         </div>
                         <p className="text-sm bg-secondary rounded-md p-2 line-clamp-4 max-w-fit">Here is all about the channel dude just have a look at meh </p>
                         <Button>Subscribe</Button>
@@ -24,12 +47,11 @@ export default function Channel(){
                 </div>
             </header>
             <section className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-2 mt-2">
-                <VideoCard/>
-                <VideoCard/>
-                <VideoCard/>
-                <VideoCard/>
-                <VideoCard/>
-                <VideoCard/>
+              {
+                creator.videos.map(({id,thumbnailUrl,title,views,createdAt})=>(
+                    <VideoCard key={id} id={id} thumbnailUrl={thumbnailUrl} title={title} views={views} createdAt={createdAt} avatar={creator.avatar} username={creator.username} />
+                ))
+              }
             </section>
         </div>
     )
