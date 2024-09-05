fb89be7cdc82437497445fa042dc345c7941b3d4
sidharth tripathi <sidharthtripathi807@gmail.com>
2024-09-05 10:49:05 +0530

    add backend for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,28 @@
 
 import { VideoCard } from "@/components/ui/VideoCard"
+import { prisma } from "@/lib/prisma"
 
-export default function Component() {
+export default async function Component() {
+  const videos = await prisma.video.findMany({
+    orderBy : {createdAt : "desc"},
+    take : 50,
+    select : {
+      thumbnailUrl : true,
+      id:true,
+      title : true,
+      views : true,
+      createdAt : true,
+      uploader : {
+        select : {username : true,avatar : true}
+      }
+    }
+
+  })
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4" >
-     <VideoCard/>
-     <VideoCard/>
-     <VideoCard/>
-     <VideoCard/>
-     <VideoCard/>
-     <VideoCard/>
-     <VideoCard/>
-     <VideoCard/>
-     <VideoCard/>
-     <VideoCard/>
-     <VideoCard/>
+      {
+        videos.map(video=>(<VideoCard id={video.id} key={video.id} thumbnailUrl={video.thumbnailUrl} avatar={video.uploader.avatar} username={video.uploader.username} createdAt={video.createdAt}title={video.title} views={video.views} />))
+      }
     </div>
   )
 }
