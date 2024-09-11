5508887ed2db01fac2567d88a6b3cae66f41c25d
sidharth tripathi <sidharthtripathi807@gmail.com>
2024-09-11 18:08:59 +0530

    fix redirection error

--- middleware.ts
+++ middleware.ts
@@ -5,14 +5,14 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req:NextRequest){
     const accessToken = cookies().get('access-token')
     const requestHeaders = new Headers(req.headers);
-    if(!accessToken) return NextResponse.redirect("http://localhost:3000/join")
+    if(!accessToken) return NextResponse.redirect(new URL("/join",req.url))
     else{
 
         try {
             const {username} = jwt.verify(accessToken.value,process.env.JWT_SECRET as string) as JwtPayload
             requestHeaders.set('username',username)
         } catch (error) {
-            return NextResponse.redirect("http://localhost:3000/join")
+            return NextResponse.redirect(new URL("/join",req.url))
         }
         finally{
             return NextResponse.next({
